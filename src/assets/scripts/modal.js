fa1ce985de919ea94db3a1b87d6b0d856f1e2c57
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2022-12-21 12:07:39 +0300

    feat: add validation for card info

--- src/assets/scripts/modal.js
+++ src/assets/scripts/modal.js
@@ -1,7 +1,10 @@
+const button = document.querySelector('.btn_confirm');
 //для правильного форматирования кредитной карты
 
 const cardNum = document.getElementById('card-number');
 const errorCard = document.querySelector('.error-card');
+// const cardLogo = document.querySelector(".card-logo");
+// const cardLogoContainer = document.querySelector(".logo-card-container");
 
 cardNum.onkeyup = function (e) {
     if (this.value == this.lastValue) return;
@@ -23,33 +26,86 @@ cardNum.onkeyup = function (e) {
 
     this.value = this.lastValue = parts.join(' ');
     this.selectionStart = this.selectionEnd = caretPosition;
-    function checkCard(){
 
-    if(cardNum.value.length === 19){
-        cardNum.style.background = 'green';
-        errorCard.style.display = 'none';
-    }else {
-        cardNum.style.background = 'red';
-        errorCard.innerHTML = 'Error';
-        errorCard.style.display = 'block';
-    }
+    function checkCard() {
+
+        if (cardNum.value.length === 19) {
+            cardNum.style.background = 'green';
+            errorCard.style.display = 'none';
+        } else {
+            cardNum.style.background = 'red';
+            errorCard.innerHTML = 'Error';
+            errorCard.style.display = 'block';
+        }
     }
 
     cardNum.addEventListener('input', checkCard);
     button.addEventListener('click', checkCard);
 }
 
+// блок ввода срока действия карты
+const expInput = document.querySelector('.exp_date');
+const errorDate = document.querySelector('.error-date');
+
+function validateExp() {
+    if (expInput.value.length === 2) {
+        expInput.value += '/';
+    }
+    if (!validate(expInput.value)) {
+        expInput.style.background = 'red';
+        errorDate.innerHTML = 'Wrong card expiration date !';
+        errorDate.style.display = 'block';
+    } else {
+        expInput.style.background = 'green';
+        errorDate.style.display = 'none';
+    }
+}
+
+function validate(value) {
+    const currentYear = (new Date().getFullYear()).toString().slice(-2);
+    let [part1, part2] = value.split('/');
+    return (value[2] === '/' && value.length === 5 && part1 <= 12 && part2 >= currentYear);
+}
+
+
+
+expInput.addEventListener('input', validateExp);
+button.addEventListener('click', validateExp);
+
+const inputCVV = document.querySelector('.input_cvv');
+const errorCVV = document.querySelector('.error-cvv');
+
+const CVV_REGEXP = /\d{3,3}/;
+
+function validateCVV() {
+
+    if (!valid(inputCVV.value)) {
+        inputCVV.style.background = 'red';
+        errorCVV.innerHTML = 'Wrong CVV !';
+        errorCVV.style.display = 'block';
+    } else {
+        inputCVV.style.background = 'green';
+        errorCVV.style.display = 'none';
+    }
+}
+
+function valid(value) {
+    return CVV_REGEXP.test(value);
+}
+
+inputCVV.addEventListener('input', validateCVV);
+button.addEventListener('click', validateCVV);
+
 //валидация email инпута
 //из ТЗ - "E-mail". Валидация: проверяется, является ли введенный текст электронной почтой
 const inputEmail = document.querySelector('.input_email');
-const button = document.querySelector('.btn_confirm');
 
 const errorName = document.querySelector('.error-name');
 const errorTel = document.querySelector('.error-telephone');
 const errorAddress = document.querySelector('.error-address');
 const errorEmail = document.querySelector('.error-email');
 
-const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 
 function onInput() {
     if (isEmailValid(inputEmail.value)) {
@@ -57,7 +113,7 @@ function onInput() {
         errorEmail.style.display = 'none';
     } else {
         inputEmail.style.background = 'red';
-        errorEmail.innerHTML = 'Error';
+        errorEmail.innerHTML = 'Wrong Email !';
         errorEmail.style.display = 'block';
     }
 }
@@ -85,7 +141,7 @@ const handleName = () => {
     if (!checkName(value)) {
 
         inputName.style.background = 'red';
-        errorName.innerHTML = 'Error';
+        errorName.innerHTML = 'Wrong Name !';
         errorName.style.display = 'block';
 
     } else {
@@ -101,7 +157,7 @@ const handleName = () => {
 }
 
 function checkName(name) {
-    return name.split(' ').every(el => el.length >= 3)
+    return name.split(' ').every(el => el.length >= 3);
 }
 
 inputName.addEventListener('input', handleName);
@@ -123,7 +179,7 @@ function onInputTel() {
         errorTel.style.display = 'none';
     } else {
         phoneInput.style.background = 'red';
-        errorTel.innerHTML = 'Error';
+        errorTel.innerHTML = 'Wrong phone number !';
         errorTel.style.display = 'block';
     }
 }
@@ -148,10 +204,9 @@ const handleForAddress = () => {
     const value = inputAddress.value.trim();
     if (!checkAddress(value)) {
         inputAddress.style.background = 'red';
-        errorAddress.innerHTML = 'Error';
+        errorAddress.innerHTML = 'Wrong address !';
         errorAddress.style.display = 'block';
     } else {
-
 
         if (!/\b\w+\b(?:.*?\b\w+\b){2}/.test(value)) {
             inputAddress.style.background = 'red';
@@ -159,6 +214,7 @@ const handleForAddress = () => {
             inputAddress.style.background = 'green';
             errorAddress.style.display = 'none';
         }
+
     }
 }
 
@@ -172,10 +228,27 @@ button.addEventListener('click', handleForAddress);
 
 //логика перехода на главную страницу. После оформления заказа
 //Из ТЗ - > при успешном прохождении валидации всех полей и нажатии на кнопку, выводится сообщение, что заказ оформлен. Затем, спустя 3-5 секунд происходит редирект на главную страницу магазина. Корзина при этом очищается
-function messageOrderIsProcessed(){
+const buttonOrder = document.querySelector(".btn_order");
+
+function checkValid(){
+
+    if (expInput.style.background === 'green' && inputCVV.style.background === 'green' 
+    && cardNum.style.background === 'green' && inputEmail.style.background === 'green'
+    && inputName.style.background === 'green' && phoneInput.style.background === 'green'
+    && inputAddress.style.background === 'green') {
+        buttonOrder.removeAttribute('disabled');
+    }
+
+}
+
+button.addEventListener('click', checkValid);
+
+function messageOrderIsProcessed() {
     alert('Заказ оформлен!');
     localStorage.clear();
     setTimeout(() => window.location.href = 'index.html', 3000);
 }
+
+buttonOrder.addEventListener('click', messageOrderIsProcessed)
 
 // messageOrderIsProcessed()
